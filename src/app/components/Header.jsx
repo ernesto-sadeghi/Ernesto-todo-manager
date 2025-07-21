fc25bfa3d645351908dc1ab1fd61fef2d0ff92ff
Ernesto <ernestosadeghi@gmail.com>
2025-07-21 13:28:11 +0330

    making the design more responsive

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -25,7 +25,7 @@ return (   <header className="bg-white border-b border-gray-200">
   
       <div className="flex items-center space-x-4">
     
-        <span className="inline-flex items-center px-4 py-2 rounded-full text-sm font-bold  bg-gradient-to-r from-amber-200 to-indigo-100  text-blue-800">
+        <span className="inline-flex items-center px-2 py-1 sm:px-4 sm:py-2 rounded-full text-sm font-bold  bg-gradient-to-r from-amber-200 to-indigo-100  text-blue-800">
         email : {loginInfo.userInfo.email}
         </span>
         
